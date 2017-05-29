106bb12c7507c814d9ba41d28b057d7c058301bf
YotamG <yotam@experitest.com>
2017-05-29 22:37:09 +0300

    Add external library and log configuration

--- src/firstClass.java
+++ src/firstClass.java
@@ -1,9 +1,15 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Created by YotamG on 29/05/2017.
  */
 public class firstClass {
 
+    private static final Logger log = LogManager.getLogger();
+
     public static void main(String[] args) {
+        log.info("This is My first log !");
         System.out.println("My First Project");
     }
 }
